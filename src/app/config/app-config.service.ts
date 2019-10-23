bbc53347fc38934162d54be03e1057e549f3a20a
Edy Segura <edysegura@gmail.com>
2019-10-22 22:31:01 -0300

    Implement config service

---
+++ src/app/config/app-config.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppConfigService {
+  private config: any;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  loadAppConfig(): Promise<any> {
+    const assignConfig = (config: any) => this.config = config;
+
+    return this.http
+      .get('/assets/config.json')
+      .toPromise()
+      .then(assignConfig);
+  }
+
+  getBaseEndpoint(): string {
+    return this.config.baseEndpoint;
+  }
+}
